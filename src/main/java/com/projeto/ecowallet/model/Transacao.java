3e02791ab9f2fc4f1faf6cbb2cfc3015ce55dc78
Isabely Xavier <isabelyxavier48@gmail.com>
2026-05-23 12:51:58 -0300

    classe abstrata transacao

---
+++ src/main/java/com/projeto/ecowallet/model/Transacao.java
@@ -0,0 +1,33 @@
+package com.projeto.ecowallet.model;
+
+import java.time.LocalDate;
+
+public abstract class Transacao {
+    private String descricao;
+    private double valor;
+    private LocalDate data;
+    private Categoria categoria;
+
+    public Transacao(String descricao, double valor, LocalDate data, Categoria categoria){
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+        this.categoria = categoria;
+    }
+    
+    public String getDescricao(){
+        return descricao;
+    }
+    public double getValor(){
+        return valor;
+    }
+    public LocalDate getData(){
+        return data;
+    }
+
+    public Categoria getCategoria(){
+        return categoria;
+    }
+
+    public abstract double getValorParaSaldo();
+}
