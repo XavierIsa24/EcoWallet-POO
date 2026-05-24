57f76651ef4922ad149ff1865fe2ca08235f69a8
milenaprama <milena.lorenzo137@gmail.com>
2026-05-24 20:27:00 -0300

    Feat: Atualiza a classe factory.

--- src/main/java/com/projeto/ecowallet/factory/TransacaoFactory.java
+++ src/main/java/com/projeto/ecowallet/factory/TransacaoFactory.java
@@ -1,28 +1,21 @@
 package com.projeto.ecowallet.factory;
 
+import com.projeto.ecowallet.model.*;
+
 import java.time.LocalDate;
 
-import com.projeto.ecowallet.model.Categoria;
-import com.projeto.ecowallet.model.Despesa;
-import com.projeto.ecowallet.model.Receita;
-import com.projeto.ecowallet.model.Transacao;
-
 public class TransacaoFactory {
-    public static Transacao criarTransacao(
-        String tipo,
-        String descricao,
-        double valor, 
-        LocalDate data,
-        Categoria categoria
-    
-    ){
 
-        if (tipo.equalsIgnoreCase("Entrada")){
-            return new Receita(descricao, valor, data, categoria);
-        } else if (tipo.equalsIgnoreCase("Saída")) {
-            return new Despesa(descricao, valor, data, categoria);
-        } else {
-            throw new IllegalArgumentException("Tipo de transação inválida.");
+    public static Transacao criarTransacao(String descricao, double valor,
+                                           LocalDate data, TipoTransacao tipo,
+                                           Categoria categoria) {
+        switch (tipo) {
+            case RECEITA:
+                return new Receita(descricao, valor, data, categoria);
+            case DESPESA:
+                return new Despesa(descricao, valor, data, categoria);
+            default:
+                throw new IllegalArgumentException("Tipo de transação inválido");
         }
     }
 }
