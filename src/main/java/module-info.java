8def9200911fff4fa077dce1d81c81c09762c2d1
milenaprama <milena.lorenzo137@gmail.com>
2026-05-24 20:28:19 -0300

    Feat: Atualiza o modulo-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,20 @@ module com.projeto.ecowallet {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.datatype.jsr310;
+
     opens com.projeto.ecowallet to javafx.fxml;
     opens com.projeto.ecowallet.controller to javafx.fxml;
+    opens com.projeto.ecowallet.model to
+            com.fasterxml.jackson.databind,
+            com.fasterxml.jackson.annotation;
 
     exports com.projeto.ecowallet;
     exports com.projeto.ecowallet.controller;
+    exports com.projeto.ecowallet.model;
+    exports com.projeto.ecowallet.factory;
+    exports com.projeto.ecowallet.persistence;
 }
